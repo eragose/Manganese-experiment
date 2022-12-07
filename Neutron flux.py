4645da2da1c1bbf04ed4f350e1ddc78ad543ef0e
eragose <erik.o.linder@gmail.com>
2022-12-07 12:57:36 +0100

    efficiency calibration done

---
+++ Neutron flux.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+#equilibrium activity in detector
+ea = 8.81223129e+05
+eaErr = 3.19415134e+03
+
+radc = 2.3 #cm
+radcErr = 0.3 #+-cm
+detRad = 7.8/2 #cm
+detRadErr = 0.5/2 #+-cm
